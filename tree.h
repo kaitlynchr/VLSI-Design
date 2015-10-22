4e8102afea0990cc5cc60d29bc65fa57449a5d59
kaitlynchr <kaitlyn.christensen@aggiemail.usu.edu>
2015-10-21 20:12:01 -0600

    Create tree.h

---
+++ tree.h
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+using namespace  std;
+struct node
+{
+        char key_value;
+        double height;
+        double width;
+        node *left;
+        node *right;
+};
