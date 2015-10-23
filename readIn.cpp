eda81266e4c7712484d3b420e7dfc5172fbb633e
kaitlynchr <kaitlyn.christensen@aggiemail.usu.edu>
2015-10-22 20:21:38 -0600

    Update readIn.cpp

--- readIn.cpp
+++ readIn.cpp
@@ -2,12 +2,13 @@
 #include <fstream>
 #include <sstream>
 #include <string>
-void readin(char *value, double *width, double *height){
+void readin(vector <char> *value, vector <double> *width, vector <double> *height){
 	//char value[100];
-	vector <char>  testValue;
-	vector <double> testWidth, testHeight;
-	double areaNode[100];
-	double ratio[100];
+	//vector <char>  testValue;
+	//vector <double> testWidth, testHeight;
+	char testValue; 
+	double areaNode;
+	double ratio;
 	ifstream myfile;
 	myfile.open("input.txt"); //file in in paranthesis
 	int i=0;
@@ -17,31 +18,27 @@ void readin(char *value, double *width, double *height){
 	if(mystring.size()<5)
 		continue;
 		istringstream buffer(mystring);
-		buffer>>value[i]>>areaNode[i]>>ratio[i];
-		testValue.push_back('V');
-		testValue.push_back(value[i]);
-		//cout<<value[i]<<" "<<areaNode[i]<<" "<<ratio[i]<<endl;
-		//value[i+1]='V';
-		//i=i+2;
-		i++;
+		buffer>>testvalue>>areaNode>>ratio;
+		value.push_back('V');
+		value.push_back(value);
+		width.push_back(areaNode);
+		height.push_back(ratio);
 
 	}
 	myfile.close();
-        for(int w=0; w<20; w++)
+        for(int w=0; w<height.size(); w++)
         {
-                width[w]=sqrt(areaNode[w]*ratio[w]);
-                height[w]=areaNode[w]/width[w];
-		testWidth.push_back(width[w]);
-		testHeight.push_back(height[w]);
+                width[w]=sqrt(width[w]*height[w]);
+                height[w]=height[w]/width[w];
         }
-/*	for ( int l=0; l<testWidth.size(); l++)
+	for ( int l=0; l<testWidth.size(); l++)
 	{
-		cout<<testValue[2*l]<<" "<<testValue[2*l+1]<<" "<<testWidth[l]<<" " <<testHeight[l]<<endl;
-	}*/
-	testValue.erase(testValue.end()-2);
-	for(int q=0; q<testValue.size(); q++)
+		cout<<value[2*l]<<" "<<value[2*l+1]<<" "<<width[l]<<" " <<height[l]<<endl;
+	}
+	value.erase(value.end()-2);
+	for(int q=0; q<value.size(); q++)
 	{
-	cout<<testValue[q]<<" ";
+	cout<<value[q]<<" ";
 	}
 return;
 }
