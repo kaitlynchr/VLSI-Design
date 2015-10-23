373861cdbf1cd1ff488474fe7fc54b3bdf7b1fed
kaitlynchr <kaitlyn.christensen@aggiemail.usu.edu>
2015-10-22 20:55:33 -0600

    Update readIn.cpp

--- readIn.cpp
+++ readIn.cpp
@@ -3,15 +3,11 @@
 #include <sstream>
 #include <string>
 void readin(vector<char>& value, vector<double>& width, vector<double>& height){
-	//char value[100];
-	//vector <char>  testValue;
-	//vector <double> testWidth, testHeight;
 	char testValue;
 	double areaNode;
 	double ratio;
 	ifstream myfile;
 	myfile.open("input.txt"); //file in in paranthesis
-	int i=0;
 	string mystring;
 	while (getline(myfile,mystring))
 	{
@@ -31,15 +27,8 @@ void readin(vector<char>& value, vector<double>& width, vector<double>& height){
                 width[w]=sqrt(width[w]*height[w]);
                 height[w]=height[w]/width[w];
         }
-	for ( int l=0; l<width.size(); l++)
-	{
-		cout<<value[2*l]<<" "<<value[2*l+1]<<" "<<width[l]<<" " <<height[l]<<endl;
-	}
+	
 	value.erase(value.end()-2);
-	for(int q=0; q<value.size(); q++)
-	{
-	cout<<value[q]<<" ";
-	}
 return;
 }
 
