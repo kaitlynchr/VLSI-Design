10c603d5d9342f7d65d91dd26cc21a53279fef41
kaitlynchr <kaitlyn.christensen@aggiemail.usu.edu>
2015-10-29 16:29:38 -0600

    Update tree.h

--- tree.h
+++ tree.h
@@ -17,3 +17,4 @@ void createTree(node *pointer, int& size, vector<char>& tree);
 double areaFunct(vector<double>& right, vector<double>& left, node *pointer);
 void readin(vector<char>& value, vector<double>& width, vector<double>& height);
 void annealingFunc(vector<char>& Enot, vector<char>& value, vector<double>& width, vector<double>& height);
+void deleteTree(node *pointer);
